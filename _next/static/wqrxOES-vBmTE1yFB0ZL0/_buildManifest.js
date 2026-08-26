c21540e75071f6c5574456b198adfc58c78464fa
infinitegame <infinitegame@infinitegamedeMacBook-Air.local>
2026-08-26 23:24:43 +0800

    Publish WeChat monthly agent showcase

---
+++ _next/static/wqrxOES-vBmTE1yFB0ZL0/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/wechat-monthly-agent//_next/:path+",
+        "destination": "/wechat-monthly-agent/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
